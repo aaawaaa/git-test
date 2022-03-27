52375ab88d0b906a25dede84c51dad829108d55d
hhh <hhh@qq.com>
2022-03-27 16:40:07 +0800

    hot-fix commit1

--- src/main/java/com/hhh/git/GitTest.java
+++ src/main/java/com/hhh/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hhh");
         System.out.println("git2");
+        System.out.println("git4");
     }
 }
